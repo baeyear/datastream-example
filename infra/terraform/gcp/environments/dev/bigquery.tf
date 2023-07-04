5e79033d7a7516e9f2d1b288b27ac9268c4602db
Yuki Kitaya <y_kitaya@yumemi.co.jp>
2023-07-04 23:07:21 +0900

    feat: bigqueryの修正

--- infra/terraform/gcp/environments/dev/bigquery.tf
+++ infra/terraform/gcp/environments/dev/bigquery.tf
@@ -1,44 +1,12 @@
-// https://registry.terraform.io/modules/terraform-google-modules/bigquery/google/latest
-module "bigquery" {
-  source  = "terraform-google-modules/bigquery/google"
-  version = "~> 6.1"
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id    = "datastream_dataset"
+  friendly_name = "datastream_dataset"
+  description   = "dataset for datastream demo"
+  project       = var.project_id
 
-  dataset_id   = "datastream_dataset"
-  dataset_name = "datastream_dataset"
-  description  = "dataset for datastream demo"
-  project_id   = var.project_id
+  location = var.default_region
 
-  tables = [
-    {
-      table_id           = "users",
-      schema             = file("bigquery_schema/user.json"),
-      time_partitioning  = null
-      range_partitioning = null,
-      expiration_time    = null,
-      clustering         = [],
-      labels = {
-        env = "dev"
-      },
-    },
-    {
-      table_id = "users_partitioned",
-      schema   = file("bigquery_schema/user.json")
-      time_partitioning = {
-        type                     = "DAY",
-        field                    = "created_at",
-        require_partition_filter = true,
-        expiration_ms            = null,
-      },
-      range_partitioning = null
-      expiration_time    = null,
-      clustering         = [],
-      labels = {
-        env = "dev"
-      }
-    },
-  ]
-
-  dataset_labels = {
+  labels = {
     env = "dev"
   }
 }
